4a5b325d3c765f1a20528fcc0eba4e77a4973c6b
mikosik <marcin.mikosik@gmail.com>
2013-12-31 15:22:09 +0100

    redesigned games.svg - main logo

--- svg/convert-all-to-png.sh
+++ svg/convert-all-to-png.sh
@@ -5,6 +5,6 @@ IMG_DIR="${SVG_DIR}"/../img
 
 cd "${SVG_DIR}"
 
-inkscape --export-width=192 --export-height=64 --export-png="${IMG_DIR}"/games.png games.svg 
+inkscape --export-width=256 --export-height=64 --export-png="${IMG_DIR}"/games.png games.svg 
 png-squeeze-full "${IMG_DIR}"/games.png
 
