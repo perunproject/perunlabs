bf4d84b9626cd9a50ec263e9a2f14322adfff4e2
mikosik <marcin.mikosik@gmail.com>
2013-12-31 12:55:49 +0100

    added better logo

---
+++ svg/convert-all-to-png.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SVG_DIR=`dirname ${0}`
+IMG_DIR="${SVG_DIR}"/../img
+
+cd "${SVG_DIR}"
+
+inkscape --export-width=192 --export-height=64 --export-png="${IMG_DIR}"/games.png games.svg 
+png-squeeze-full "${IMG_DIR}"/games.png
+
