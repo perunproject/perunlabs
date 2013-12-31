65b48daab72f5959117022cc157e22027628d9c4
mikosik <marcin.mikosik@gmail.com>
2013-12-31 19:25:44 +0100

    redesigned sokoban box

--- svg/convert-all-to-png.sh
+++ svg/convert-all-to-png.sh
@@ -8,3 +8,6 @@ cd "${SVG_DIR}"
 inkscape --export-width=256 --export-height=64 --export-png="${IMG_DIR}"/games.png games.svg 
 png-squeeze-full "${IMG_DIR}"/games.png
 
+inkscape --export-width=500 --export-height=250 --export-png="${IMG_DIR}"/sokoban.png sokoban.svg
+png-squeeze-full "${IMG_DIR}"/sokoban.png
+
